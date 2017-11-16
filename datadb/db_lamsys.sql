4bb4dbcfaf63372e496f0a83529840adf846c3dd
Lauren Mejia Garcia <Lauren Mejia Garcia>
2017-11-16 13:39:10 -0600

    scritp data

---
+++ datadb/db_lamsys.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-11-2017 a las 20:26:00
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_lamsys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulo`
+--
+
+CREATE TABLE `articulo` (
+  `idarticulo` int(11) NOT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `codigo` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` varchar(512) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `imagen` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `estado` varchar(20) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`idarticulo`, `idcategoria`, `codigo`, `nombre`, `stock`, `descripcion`, `imagen`, `estado`) VALUES
+(1, 4, '001', 'Mapa de recorrido', 12, 'test', 'etapa1.PNG', 'Inactivo'),
+(2, 1, '0001', 'Mouse', 10, 'Mouse para computadoras', NULL, 'Activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` varchar(256) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `condicion` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `nombre`, `descripcion`, `condicion`) VALUES
+(1, 'Equipos de Computos', 'Accesorios de computadoras y electronica', 1),
+(2, 'Limpieza', 'Articulos de limpiezas', 0),
+(3, 'Utiles Escolares', 'Utiles y accesesorios escolares', 1),
+(4, 'Vestimenta', 'Articulos y prendas de vestir', 1),
+(5, 'Abarrotes', 'Productos comestibles ', 1),
+(6, 'Herramientas', 'Articuloes y accesorios de ferreteria', 1),
+(12, 'Pinturas', 'Pinturas y accesorios para pintar', 1),
+(13, 'Ceramicas', 'Ladrillos y ceramicas ', 0),
+(14, 'Lacteos', 'Productos elaborados a base de leche de vaca', 1),
+(15, 'Frutas22', 'Frutas organicas', 1),
+(16, 'Tecnologia', 'Aparatos tecnologicos', 0),
+(17, 'test', 'test', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ingreso`
+--
+
+CREATE TABLE `detalle_ingreso` (
+  `iddetalle_ingreso` int(11) NOT NULL,
+  `idingreso` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_compra` decimal(11,2) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `iddetalle_venta` int(11) NOT NULL,
+  `idventa` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingreso`
+--
+
+CREATE TABLE `ingreso` (
+  `idingreso` int(11) NOT NULL,
+  `idproveedor` int(11) NOT NULL,
+  `tipo_comprobante` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `serie_comprobante` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `num_comprobante` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(10,0) NOT NULL,
+  `estado` varchar(45) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `idpersona` int(11) NOT NULL,
+  `tipo_persona` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `tipo_documento` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `num_documento` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `direccion` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `telefono` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_spanish2_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`idpersona`, `tipo_persona`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`) VALUES
+(1, 'Cliente', 'Lauren Mejia', 'CI', '3622908920000L', 'Managua', '84050372', 'mejialauren29@gmail.com'),
+(2, 'Cliente', 'gabriela', 'CI', '556565', 'managua', '8448', 'test'),
+(3, 'Proveedor', 'CONSUTECSA', 'RUC', '3655545gg', 'Managua', '8484848', 'lmejia@consutecsa.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `idventa` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `tipo_comprobante` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `serie_comprobante` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `num_comprobante` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total_venta` decimal(11,2) NOT NULL,
+  `estado` varchar(45) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`idarticulo`),
+  ADD KEY `fk_articulo_categoria_idx` (`idcategoria`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD PRIMARY KEY (`iddetalle_ingreso`),
+  ADD KEY `fk_detalle_ingreso_idx` (`idingreso`),
+  ADD KEY `fk_detalle_articulo_idx` (`idarticulo`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`iddetalle_venta`),
+  ADD KEY `fk_detalle_venta_idx` (`idarticulo`),
+  ADD KEY `fk_detalleVenta_venta_idx` (`idventa`);
+
+--
+-- Indices de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD PRIMARY KEY (`idingreso`),
+  ADD KEY `fk_ingreso_persona_idx` (`idproveedor`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idventa`),
+  ADD KEY `fk_venta_cliente_idx` (`idcliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  MODIFY `iddetalle_ingreso` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `iddetalle_venta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  MODIFY `idingreso` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idventa` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD CONSTRAINT `fk_articulo_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD CONSTRAINT `fk_detalle_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ingreso` FOREIGN KEY (`idingreso`) REFERENCES `ingreso` (`idingreso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `fk_detalleVenta_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalleVenta_venta` FOREIGN KEY (`idventa`) REFERENCES `venta` (`idventa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD CONSTRAINT `fk_ingreso_persona` FOREIGN KEY (`idproveedor`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`idcliente`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
