dd4203e99cc69d75b60a63a0817e9e2977e1ecf3
mejialauren29 <mejialauren29@gmail.com>
2017-11-24 11:31:14 -0600

    actualizacion y nuevo controlador IngresoController

--- app/Http/Requests/IngresoFormRequest.php
+++ app/Http/Requests/IngresoFormRequest.php
@@ -24,11 +24,14 @@ class IngresoFormRequest extends Request
     public function rules()
     {
         return [
-            'idproveedor'=>'required'
-            'tipo_comprobante'=>'required'
-             'idproveedor'=>'required'
-               'idproveedor'=>'required'
-                'idproveedor'=>'required'
+            'idproveedor'=>'required',
+            'tipo_comprobante'=>'required|max:20',
+            'serie_comprobante'=>'max:7',
+            'num_comprobante'=>'required|max:10',
+            'idarticulo'=>'required',
+            'cantidad'=>'required',
+            'precio_compra'=>'required',
+            'precio_venta'=>'required'
         ];
     }
 }
