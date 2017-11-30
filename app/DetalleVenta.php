843d5fd3e6e4c4c90fe4ed34068e900e275e5307
mejialauren29 <mejialauren29@gmail.com>
2017-11-29 22:48:03 -0600

    add controller and new view for ingreso

---
+++ app/DetalleVenta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lamsys;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleVenta extends Model
+{
+    //
+}
---
+++ app/Venta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lamsys;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venta extends Model
+{
+    //
+}
